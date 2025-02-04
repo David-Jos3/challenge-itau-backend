ba1cd298ef060c9aac1486737547b754a7229e99
David-jos3 <davidgomesgc230@gmail.com>
2025-02-04 20:51:51 -0300

    feat: creating the transaction use case

---
+++ src/domain/entities/transaction.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from "node:crypto"
+import { Entity } from "./entity"
+
+export interface  TransactionProps {
+  value: number
+  dateTime: string
+}
+
+export class Transaction extends Entity<TransactionProps> {
+  private constructor( props: TransactionProps, id?: string) {
+    super(props, id)
+  }
+  public static create(props: TransactionProps, id?: string): Transaction {
+   return new Transaction(props, id)
+  } 
+
+  get transationId() {
+    return this.id
+  }
+
+  get value() {
+    return this.props.value
+  }
+
+  set value(num: number) {
+    this.props.value = num
+  }
+
+  get dateTime() {
+    return this.props.dateTime
+  }
+
+  set dateTime(date: string) {
+    this.props.dateTime= date
+  }
+
+}
