7afcc517fda9f9ebb69b0bf16dee670a9645f324
David-jos3 <davidgomesgc230@gmail.com>
2025-02-08 11:29:35 -0300

    feat: register transaction and statistic routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import fastify, {FastifyRequest, FastifyReply} from "fastify";
-
+import fastify, { FastifyRequest, FastifyReply } from 'fastify'
+import { transactionRouter } from './infra/controllers/transaction/router'
+import { statisticRouter } from './infra/controllers/statistic/router'
 
 export const app = fastify({ logger: true })
 
-app.get('/hello', (request:FastifyRequest, reply:FastifyReply) => {
-  reply.send({hello: 'Hello Itaú'})
-})
+app.register(transactionRouter)
+app.register(statisticRouter)
 
+app.get('/hello', (request:FastifyRequest, reply:FastifyReply) => {
+  reply.send({ hello: 'Hello Itaú' })
+})
